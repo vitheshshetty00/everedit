c2f461fa87439ec53c979cbba5f1ce56cdc68d64
Vithesh Shetty <vitheshshetty23@gmail.com>
2024-07-24 15:16:25 +0530

    feat:Added liveblocks

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,24 +1,11 @@
-import Header from "@/components/Header";
-import { Editor } from "@/components/editor/Editor";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
+
 
 const Document = () => {
 	return (
-		<div>
-			<Header>
-				<div className="flex w-fit items-center justify-center gap-2">
-					<p className="document-title">Share</p>
-				</div>
-				<SignedOut>
-					<SignInButton />
-				</SignedOut>
-				<SignedIn>
-					<UserButton />
-				</SignedIn>
-			</Header>
-
-			<Editor />
-		</div>
+		<main className="flex w-full flex-col items-center">
+			<CollaborativeRoom/>
+		</main>
 	);
 };
 
